c8899fac0da161718522f60ff51eacbc3e2b566a
luanaduque <luanaduque273@gmail.com>
2023-10-10 21:17:43 -0300

    finalizando editarProdutos

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -32,3 +32,49 @@ export default function EditarProdutos() {
     const { name, value } = e.target;
     setProduto({ ...produto, [name]: value });
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch(`http://localhost:5000/produtos/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(produto),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        // Redirecionar após a edição ser concluída
+        navigate('/produtos');
+      })
+      .catch((error) => console.log(error));
+  }
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <legend>Editar Produto</legend>
+            <div>
+              <label htmlFor="idNome">Nome</label>
+              <input type="text" name="nome" id="idNome" value={produto.nome} onChange={handleChange} />
+            </div>
+            <div>
+              <label htmlFor="idDesc">Descrição</label>
+              <input type="text" name="desc" id="idDesc" value={produto.desc} onChange={handleChange} />
+            </div>
+            <div>
+              <label htmlFor="idPreco">Preço</label>
+              <input type="text" name="preco" id="idPreco" value={produto.preco} onChange={handleChange} />
+            </div>
+            <div>
+              <button type="submit">EDITAR</button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    </>
+  );
+}
